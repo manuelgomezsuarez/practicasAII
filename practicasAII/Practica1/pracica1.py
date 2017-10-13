f4fd23b420dd34ddd0d3bc3580e0cf9b9200bb38
manuelgomezsuarez <manuelgomezsuarez@gmail.com>
2017-10-13 16:12:00 +0200

    Puesto en funciones, puebla base datos correctamente

--- practicasAII/Practica1/pracica1.py
+++ practicasAII/Practica1/pracica1.py
@@ -13,8 +13,17 @@ import feedparser
 url="http://www.us.es/rss/feed/portada"
 rss=feedparser.parse(url)
 entradas=rss.entries
-
+def pueblaBaseDatos():
+    conn=sqlite3.connect("noticias")
+    for noticia in entradas:
+        titulo=str(noticia.title.encode('utf-8'))
+        link=str(noticia.link.encode('utf-8'))
+        fecha=str(noticia.published_parsed[2])+"/" +str(noticia.published_parsed[1])+"/" +str(noticia.published_parsed[0])
+        conn.execute("INSERT INTO NOTICIAS (TITULO,LINK,FECHA) \
+           VALUES ('"+titulo+"','"+link+"' ,'"+fecha+"')");
+        conn.commit  
 def almacenaNoticias():
+    
     conn=sqlite3.connect("noticias")
     
     try:
@@ -28,38 +37,32 @@ def almacenaNoticias():
     except:
         print("Ya estaba creada la tabla")
         pass
-    idNoticia=0
-    for noticia in entradas:
-        titulo=str(noticia.title.encode('utf-8'))
-        link=str(noticia.link.encode('utf-8'))
-        fecha=str(noticia.published_parsed[2])+"/" +str(noticia.published_parsed[1])+"/" +str(noticia.published_parsed[0])
-        conn.execute("INSERT INTO NOTICIAS (TITULO,LINK,FECHA) \
-           VALUES ('"+titulo+"','"+link+"' ,'"+fecha+"')");
-        conn.commit  
+    pueblaBaseDatos()
         
 
 #         print(titulo)
 #         print(link)
 #         print(fecha)
 #         print(idNoticia)
-        idNoticia+=1
     print("Noticias guardadas correctamente")
 
-
-ventana = tk.Tk()
-
-button_frame = tk.Frame(ventana)
-button_frame.pack(fill=tk.X, side=tk.BOTTOM)
-
-b1 = tk.Button(button_frame, text='Almacenar',command=almacenaNoticias)
-b2 = tk.Button(button_frame, text='Listar')
-b3 = tk.Button(button_frame, text='Buscar')
-
-button_frame.columnconfigure(0, weight=1)
-button_frame.columnconfigure(1, weight=1)
-button_frame.columnconfigure(2, weight=1)
-
-b1.grid(row=0, column=0, sticky=tk.W+tk.E)
-b2.grid(row=0, column=1, sticky=tk.W+tk.E)
-b3.grid(row=0,column=2,sticky=tk.W+tk.E)
-ventana.mainloop()
+def creaMenu():
+    ventana = tk.Tk()
+    
+    button_frame = tk.Frame(ventana)
+    button_frame.pack(fill=tk.X, side=tk.BOTTOM)
+    
+    b1 = tk.Button(button_frame, text='Almacenar',command=almacenaNoticias)
+    b2 = tk.Button(button_frame, text='Listar')
+    b3 = tk.Button(button_frame, text='Buscar')
+    
+    button_frame.columnconfigure(0, weight=1)
+    button_frame.columnconfigure(1, weight=1)
+    button_frame.columnconfigure(2, weight=1)
+    
+    b1.grid(row=0, column=0, sticky=tk.W+tk.E)
+    b2.grid(row=0, column=1, sticky=tk.W+tk.E)
+    b3.grid(row=0,column=2,sticky=tk.W+tk.E)
+    ventana.mainloop()
+    
+creaMenu()
